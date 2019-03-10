8a772676f236bbe5d013bb4375db010c39a4b579
Neha Upadhyay <upaneha1212@gmail.com>
2019-03-11 01:13:47 +0530

    Updated nodule file

--- employee-portal/projects/emp-shared/src/lib/emp-shared.module.ts
+++ employee-portal/projects/emp-shared/src/lib/emp-shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { EmpSharedComponent } from './emp-shared.component';
 import { CurrentUserComponent } from './current-user/current-user.component';
-import { UserDetailsService } from './user-details.service';
 
 @NgModule({
   declarations: [EmpSharedComponent, CurrentUserComponent],
   imports: [
   ],
-  providers: [UserDetailsService],
+  providers: [],
   exports: [EmpSharedComponent, CurrentUserComponent]
 })
 export class EmpSharedModule { }
